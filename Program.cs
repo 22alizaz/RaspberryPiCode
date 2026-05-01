bf591fd89b7e70303e78902cb102e81e7761ba97
Aliza Zubair <22AZubair@heckgrammar.co.uk>
2026-05-01 12:16:40 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,26 +1,31 @@
 ﻿using System.Device.Gpio;
-using System.Runtime.ExceptionServices;
 namespace RaspberryPiCode
 {
     internal class Program
     {
-    static void wipe(GpioController controller, int[] pins)
+        static void wipe(GpioController controller, int[] pins)
         {
             for (int i = 0; i < pins.Length; i++)
             {
                 controller.OpenPin(pins[i], PinMode.Output);
-                controller.Write(pins[i], PinValue.High);
+                controller.Write(pins[i], PinValue.Low);
                 controller.ClosePin(pins[i]);
             }
         }
          
         static void Main(string[] args)
         {
-            GpioController controller = new GpioController(PinNumberingScheme.Board);
-            int[] pins = { 11, 13, 15, 29 };
-            wipe (controller, pins);
-
-            Thread.Sleep(2000);
+            GpioController controller = new GpioController(PinNumberingScheme.Board); 
+            int Greenpin = 11;
+            int yellowpin = 13;
+            int redpin = 15;
+            int bluepin = 29;
+            int[] pins = { Greenpin, yellowpin, redpin, bluepin };
+            int lightTime = 500;
+            wipe(controller, pins);
+            controller.OpenPin(pins[1], PinMode.Output);
+            controller.Write(pins[1], PinValue.High);
+            controller.ClosePin(pins[1]);
         }
 
     }
