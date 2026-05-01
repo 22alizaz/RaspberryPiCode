47c993f193923d488a1713b48c9adc851624a917
Aliza Zubair <22AZubair@heckgrammar.co.uk>
2026-05-01 11:52:12 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
 ﻿using System.Device.Gpio;
+using System.Runtime.ExceptionServices;
 namespace RaspberryPiCode
 {
     internal class Program
     {
+    static void wipe(GpioController controller, int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
+            {
+                controller.OpenPin(pins[i], PinMode.Output);
+                controller.Write(pins[i], PinValue.High);
+                controller.ClosePin(pins[i]);
+            }
+        }
+         
         static void Main(string[] args)
         {
             GpioController controller = new GpioController(PinNumberingScheme.Board);
-            int[] pins = { };
-            controller.OpenPin(40, PinMode.Output);
-            controller.Write(40, PinValue.High);
-            controller.ClosePin(40);
+            int[] pins = { 11, 13, 15, 29 };
+            wipe (controller, pins);
+
+            Thread.Sleep(2000);
         }
+
     }
 }
